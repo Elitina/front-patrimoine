02103ba2904f04b6152e6bbbeb46cff101dd3497
Elitina <hei.elitina@gmail.com>
2024-09-08 22:22:07 +0300

    feat: update vite.config.js

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: '0.0.0.0', 
     port: process.env.PORT || 3000,  
     proxy: {
-      '/patrimoines': {
+      '/patrimoine': {
         target: process.env.REACT_APP_API_URL || 'http://localhost:3000',  
         changeOrigin: true,
         secure: false,
